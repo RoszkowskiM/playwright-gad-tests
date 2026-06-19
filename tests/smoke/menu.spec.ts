4c82c68b7cfa2b26185567064863a4f629eaa0b3
RoszkowskiM <m.roszkowski88@gmail.com>
2026-06-19 10:44:48 +0200

    test: main menu smokes

---
+++ tests/smoke/menu.spec.ts
@@ -0,0 +1,66 @@
+import { ArticlesPage } from '../../src/pages/articles.page';
+import { CommentsPage } from '../../src/pages/comments.page';
+import { HomePage } from '../../src/pages/home.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify menu main buttons', () => {
+  test(
+    'comments button navigates to comments page',
+    {
+      tag: ['@GAD-R01-03'],
+    },
+    async ({ page }) => {
+      // Arrange
+      const articlesPage = new ArticlesPage(page);
+      const commentsPage = new CommentsPage(page);
+
+      // Act
+      await articlesPage.goto();
+      await articlesPage.mainMenu.commentsButton.click();
+      const title = await commentsPage.title();
+
+      // Assert
+      expect(title).toContain('Comments');
+    },
+  );
+
+  test(
+    'articles button navigates to articles page',
+    {
+      tag: ['@GAD-R01-03'],
+    },
+    async ({ page }) => {
+      // Arrange
+      const commentsPage = new CommentsPage(page);
+      const articlesPage = new ArticlesPage(page);
+
+      // Act
+      await commentsPage.goto();
+      await commentsPage.mainMenu.articlesButton.click();
+      const title = await articlesPage.title();
+
+      // Assert
+      expect(title).toContain('Articles');
+    },
+  );
+
+  test(
+    'home page button navigates to home page',
+    {
+      tag: ['@GAD-R01-03'],
+    },
+    async ({ page }) => {
+      // Arrange
+      const articlesPage = new ArticlesPage(page);
+      const homePage = new HomePage(page);
+
+      // Act
+      await articlesPage.goto();
+      await articlesPage.mainMenu.homePage.click();
+      const title = await homePage.title();
+
+      // Assert
+      expect(title).toContain('GAD');
+    },
+  );
+});
