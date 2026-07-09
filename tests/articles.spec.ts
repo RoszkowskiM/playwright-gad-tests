2a746214b12412a469713fab449dd88a8467d4db
RoszkowskiM <m.roszkowski88@gmail.com>
2026-07-09 10:11:08 +0200

    test: create and verify article (e2e)

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -25,30 +25,6 @@ test.describe('Verify articles', () => {
   });
 
   test(
-    'create new article',
-    {
-      tag: ['@GAD-R04-01'],
-    },
-    async ({ page }) => {
-      // Arrange
-      const expectedText = 'Article was created';
-
-      const articlePage = new ArticlePage(page);
-      const articleData = randomNewArticle();
-
-      // Act
-      await addArticleView.createArticle(articleData);
-
-      // Assert
-      await expect.soft(addArticleView.alertPopup).toHaveText(expectedText);
-      await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
-      await expect
-        .soft(articlePage.articleBody)
-        .toHaveText(articleData.body, { useInnerText: true });
-    },
-  );
-
-  test(
     'reject creating new article with empty title field',
     {
       tag: ['@GAD-R04-01'],
