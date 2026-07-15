343c1919ca08dab878813cdef41579df1ab6ad66
RoszkowskiM <m.roszkowski88@gmail.com>
2026-07-15 09:36:28 +0200

    chore: refactor to aaa formatting

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -14,12 +14,12 @@ test.describe('Verify login', () => {
       // Arrange
       const expectedWelcomeTitle = 'Welcome';
       const loginPage = new LoginPage(page);
+      const welcomePage = new WelcomePage(page);
 
       // Act
       await loginPage.goto();
       await loginPage.login(testUser1);
 
-      const welcomePage = new WelcomePage(page);
       const title = await welcomePage.getTitle();
 
       // Assert
