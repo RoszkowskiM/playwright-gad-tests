6c83d22f0aa1ca3d58242faa44771b38e7e5c4a4
RoszkowskiM <m.roszkowski88@gmail.com>
2026-07-30 09:43:36 +0200

    test: article integration test

---
+++ tests/integration/article.spec.ts
@@ -0,0 +1,22 @@
+import { ArticlePage } from '@_src/pages/article.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify articles', () => {
+  test(
+    'non logged user can access created article',
+    {
+      tag: ['@GAD-R06-01', '@predefined_data'],
+    },
+    async ({ page }) => {
+      // Arrange
+      const articlePage = new ArticlePage(page);
+      const expectedArticleTitle = 'How to write effective test cases';
+
+      // Act
+      articlePage.goto('?id=1');
+
+      // Assert
+      await expect(articlePage.articleTitle).toHaveText(expectedArticleTitle);
+    },
+  );
+});
