a9d54515026f60c451df85dee088db50e0f415d0
RoszkowskiM <m.roszkowski88@gmail.com>
2026-09-16 09:23:51 +0200

    test: api post article negative test

---
+++ tests/api/articles.integration.spec.ts
@@ -0,0 +1,28 @@
+import { prepareRandomArticle } from '@_src/factories/article.factory';
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify articles CRUD operations @api', () => {
+  test('should not create an article without a logged-in user', async ({
+    request,
+  }) => {
+    // Arrange
+    const expectedStatusCode = 401;
+    const articlesUrl = '/api/articles';
+
+    const randomArticleData = prepareRandomArticle();
+    const articleData = {
+      title: randomArticleData.title,
+      body: randomArticleData.body,
+      date: '2026-09-16T07:18:47.823Z',
+      image: 'image_string',
+    };
+
+    // Act
+    const response = await request.post(articlesUrl, {
+      data: articleData,
+    });
+
+    // Assert
+    expect(response.status()).toBe(expectedStatusCode);
+  });
+});
